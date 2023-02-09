18d2364ca7a2fac6acdd6af2fa366a26995ec341
Andy Newhouse <hi@andymnewhouse.me>
2023-02-08 19:56:29 -0600

    Add install command for creating collections

--- config/transistor.php
+++ config/transistor.php
@@ -4,4 +4,6 @@ return [
 
     'api_key' => env('TRANSISTOR_API_KEY'),
 
+    'show_collection' => ['title' => 'Podcast Shows', 'handle' => 'podcast-shows'],
+    'episode_collection' => ['title' => 'Podcast Episodes', 'handle' => 'podcast-episodes'],
 ];
